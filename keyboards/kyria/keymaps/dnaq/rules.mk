c66c4b7596b2d3a90e0d269b8f7c4ab3d638ba02
dnaq <dnaq@users.noreply.github.com>
2021-07-06 22:43:32 +0200

    switch back to sym_defer for debouncing

--- keyboards/kyria/keymaps/dnaq/rules.mk
+++ keyboards/kyria/keymaps/dnaq/rules.mk
@@ -2,5 +2,5 @@ OLED_DRIVER_ENABLE = no # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 COMBO_ENABLE = yes
-DEBOUNCE_TYPE = sym_eager_pk
+DEBOUNCE_TYPE = sym_defer_pk
 MOUSE_KEY_ENABLE = yes
